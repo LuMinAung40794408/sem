e4dfa469713b9bf9d0d6d068b797c83a766f3703
Lu Min Aung <lumin1558@gmail.com>
2025-11-07 03:04:31 +0630

    lab 7 AppIntegrationTest code cov final

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -27,4 +27,18 @@ public class AppIntegrationTest
         assertEquals("Ronghao", emp.first_name);
         assertEquals("Garigliano", emp.last_name);
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }

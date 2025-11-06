c11333346bf8a1814ff376a5dbc6d5eebc8f605d
Lu Min Aung <lumin1558@gmail.com>
2025-11-06 13:53:22 +0630

    lab 7 AppIntegrationTest update 3

--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -1,28 +1,38 @@
 package com.napier.devops;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppIntegrationTest
-{
+class AppIntegrationTest{
+
     static App app;
 
-    @BeforeAll
-    static void init()
-    {
+    @BeforeEach
+    void setUp() {
         app = new App();
-        app.connect("localhost:33060", 30000);
-
+        app.connect("localhost:33060", 10000);
     }
 
     @Test
-    void testGetEmployee()
-    {
+    void testGetEmployee() {
         Employee emp = app.getEmployee(255530);
-        assertEquals(emp.emp_no, 255530);
-        assertEquals(emp.first_name, "Ronghao");
-        assertEquals(emp.last_name, "Garigliano");
+        assertEquals(255530, emp.emp_no);
+        assertEquals("Ronghao", emp.first_name);
+        assertEquals("Garigliano", emp.last_name);
+    }
+
+    @Test
+    void testAddEmployee() {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Lu Min";
+        emp.last_name = "Aung";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Lu Min");
+        assertEquals(emp.last_name, "Aung");
     }
 }
